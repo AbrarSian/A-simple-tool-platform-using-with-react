9c0c8a453d46ebd3fa9df67f4f5d1f50338c3d32
AbrarSian <abrarsian96@gmail.com>
2026-04-01 21:09:23 +0600

    json data added

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,54 @@ import State_Section from './components/Banner/State_Section/State_Section'
 import Started_Section from './components/Banner/State_Section/Started_Section/Started_Section'
 import Pricing_Section from './components/Pricing_Section/Pricing_Section'
 import Ready_Section from './components/Ready_Section/Ready_Section'
+import Premium_Section from "./components/Premium_Section/Premium_Section";
+
+
+
+const cardsData = async () => {
+    const res = await fetch("/CardsData.json");
+    return res.json();
+  };
+
+  const dataPromise = cardsData();
+
 
 function App() {
   return (
     <>
-     <div className='max-w-400 mx-auto'>
-        <div className='lg:w-[85%] mx-auto'>
+     <div className="max-w-400 mx-auto">
+        <div className="lg:w-[85%] mx-auto">
           <Navbar></Navbar>
         </div>
 
-        <div className='lg:w-[85%] mx-auto'>
+        <div className="lg:w-[85%] mx-auto">
           <Banner></Banner>
         </div>
 
-        <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA]'>
+        <div className="bg-linear-to-r from-[#4F39F6] to-[#9514FA]">
         <State_Section></State_Section>
         </div>
 
-        <div className='lg:w-[85%] mx-auto'>
+        <div className="lg:w-[85%] mx-auto my-14 lg:my-24">
+          <Premium_Section dataPromise={dataPromise}></Premium_Section>
+        </div>
+
+        <div className="lg:w-[85%] mx-auto">
           <Started_Section></Started_Section>
         </div>
         
-        <div className='lg:w-[85%] mx-auto'>
+        <div className="lg:w-[85%] mx-auto">
           <Pricing_Section></Pricing_Section>
         </div>
 
-        <div className='mt-7 lg:mt-15 bg-linear-to-r from-[#4F39F6] to-[#9514FA]'>
+        <div className="mt-7 lg:mt-15 bg-linear-to-r from-[#4F39F6] to-[#9514FA]">
           <Ready_Section></Ready_Section>
         </div>
 
         <Footer></Footer>
      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
